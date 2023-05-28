f82e5ffc57f940e3649de73d44224915ef574993
Mikhail326 <misha.stelmakh1995@gmail.com>
2023-05-28 13:08:05 +0300

    lesson 1-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList, { TasksType } from './components/TodoList/TodoList';
+
+const tasks1: Array<TasksType> = [
+  {id: 1, title:'js', isDone: true},
+  {id: 2, title:'CSS', isDone: true},
+  {id: 3, title:'HTML', isDone: false}
+]
+
+const tasks2: Array<TasksType> = [
+  {id: 1, title:'No js', isDone: true},
+  {id: 2, title:'No CSS', isDone: false},
+  {id: 3, title:'No HTML', isDone: true}
+]
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <TodoList title="React-todolist-1" tasks={tasks1}/>
+      <TodoList title="React-todolist-2" tasks={tasks2}/>
     </div>
   );
 }
